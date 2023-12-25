c0550fe645466c762e2df9f9a812e3d3d44b05a9
Daniel Yim <danielyim@gmail.com>
2023-12-24 18:41:05 -0800

    Prettier

--- main.ts
+++ main.ts
@@ -1,255 +1,286 @@
 import { EditorExtensions } from "editor-enhancements";
 import { Plugin, MarkdownView, Editor } from "obsidian";
 import {
-  AutoLinkTitleSettings,
-  AutoLinkTitleSettingTab,
-  DEFAULT_SETTINGS,
+	AutoLinkTitleSettings,
+	AutoLinkTitleSettingTab,
+	DEFAULT_SETTINGS
 } from "./settings";
 import { CheckIf } from "checkif";
 import getPageTitle from "scraper";
 
 interface PasteFunction {
-  (this: HTMLElement, ev: ClipboardEvent): void;
+	(this: HTMLElement, ev: ClipboardEvent): void;
 }
 
 export default class AutoLinkTitle extends Plugin {
-  settings: AutoLinkTitleSettings;
-  pasteFunction: PasteFunction;
-  blacklist: Array<string>;
+	settings: AutoLinkTitleSettings;
+	pasteFunction: PasteFunction;
+	blacklist: Array<string>;
 
-  async onload() {
-    console.log("loading obsidian-auto-link-title");
-    await this.loadSettings();
+	async onload() {
+		console.log("loading obsidian-auto-link-title");
+		await this.loadSettings();
 
-    this.blacklist = this.settings.websiteBlacklist.split(",").map(s => s.trim()).filter(s => s.length > 0)
+		this.blacklist = this.settings.websiteBlacklist
+			.split(",")
+			.map(s => s.trim())
+			.filter(s => s.length > 0);
 
-    // Listen to paste event
-    this.pasteFunction = this.pasteUrlWithTitle.bind(this);
+		// Listen to paste event
+		this.pasteFunction = this.pasteUrlWithTitle.bind(this);
 
-    this.addCommand({
-      id: "auto-link-title-paste",
-      name: "Paste URL and auto fetch title",
-      editorCallback: (editor) => this.manualPasteUrlWithTitle(editor),
-      hotkeys: [],
-    });
+		this.addCommand({
+			id: "auto-link-title-paste",
+			name: "Paste URL and auto fetch title",
+			editorCallback: editor => this.manualPasteUrlWithTitle(editor),
+			hotkeys: []
+		});
 
-    this.addCommand({
-      id: "auto-link-title-normal-paste",
-      name: "Normal paste (no fetching behavior)",
-      editorCallback: (editor) => this.normalPaste(editor),
-      hotkeys: [
-        {
-          modifiers: ["Mod", "Shift"],
-          key: "v",
-        },
-      ],
-    });
+		this.addCommand({
+			id: "auto-link-title-normal-paste",
+			name: "Normal paste (no fetching behavior)",
+			editorCallback: editor => this.normalPaste(editor),
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Shift"],
+					key: "v"
+				}
+			]
+		});
 
-    this.registerEvent(
-      this.app.workspace.on("editor-paste", this.pasteFunction)
-    );
+		this.registerEvent(
+			this.app.workspace.on("editor-paste", this.pasteFunction)
+		);
 
-    this.addCommand({
-      id: "enhance-url-with-title",
-      name: "Enhance existing URL with link and title",
-      editorCallback: (editor) => this.addTitleToLink(editor),
-      hotkeys: [
-        {
-          modifiers: ["Mod", "Shift"],
-          key: "e",
-        },
-      ],
-    });
+		this.addCommand({
+			id: "enhance-url-with-title",
+			name: "Enhance existing URL with link and title",
+			editorCallback: editor => this.addTitleToLink(editor),
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Shift"],
+					key: "e"
+				}
+			]
+		});
 
-    this.addSettingTab(new AutoLinkTitleSettingTab(this.app, this));
-  }
+		this.addSettingTab(new AutoLinkTitleSettingTab(this.app, this));
+	}
 
-  addTitleToLink(editor: Editor): void {
-    // Only attempt fetch if online
-    if (!navigator.onLine) return;
+	addTitleToLink(editor: Editor): void {
+		// Only attempt fetch if online
+		if (!navigator.onLine) return;
 
-    let selectedText = (EditorExtensions.getSelectedText(editor) || "").trim();
+		let selectedText = (
+			EditorExtensions.getSelectedText(editor) || ""
+		).trim();
 
-    // If the cursor is on a raw html link, convert to a markdown link and fetch title
-    if (CheckIf.isUrl(selectedText)) {
-      this.convertUrlToTitledLink(editor, selectedText);
-    }
-    // If the cursor is on the URL part of a markdown link, fetch title and replace existing link title
-    else if (CheckIf.isLinkedUrl(selectedText)) {
-      var link = this.getUrlFromLink(selectedText);
-      this.convertUrlToTitledLink(editor, link);
-    }
-  }
+		// If the cursor is on a raw html link, convert to a markdown link and fetch title
+		if (CheckIf.isUrl(selectedText)) {
+			this.convertUrlToTitledLink(editor, selectedText);
+		}
+		// If the cursor is on the URL part of a markdown link, fetch title and replace existing link title
+		else if (CheckIf.isLinkedUrl(selectedText)) {
+			var link = this.getUrlFromLink(selectedText);
+			this.convertUrlToTitledLink(editor, link);
+		}
+	}
 
-  async normalPaste(editor: Editor): Promise<void> {
+	async normalPaste(editor: Editor): Promise<void> {
+		let clipboardText = await navigator.clipboard.readText();
+		if (clipboardText === null || clipboardText === "") return;
 
-    let clipboardText = await navigator.clipboard.readText();
-    if (clipboardText === null || clipboardText === "") return;
+		editor.replaceSelection(clipboardText);
+	}
 
-    editor.replaceSelection(clipboardText);
-  }
+	// Simulate standard paste but using editor.replaceSelection with clipboard text since we can't seem to dispatch a paste event.
+	async manualPasteUrlWithTitle(editor: Editor): Promise<void> {
+		// Only attempt fetch if online
+		if (!navigator.onLine) {
+			editor.replaceSelection(clipboardText);
+			return;
+		}
 
-  // Simulate standard paste but using editor.replaceSelection with clipboard text since we can't seem to dispatch a paste event.
-  async manualPasteUrlWithTitle(editor: Editor): Promise<void> {
+		var clipboardText = await navigator.clipboard.readText();
+		if (clipboardText == null || clipboardText == "") return;
 
-    // Only attempt fetch if online
-    if (!navigator.onLine) {
-      editor.replaceSelection(clipboardText);
-      return;
-    }
+		// If its not a URL, we return false to allow the default paste handler to take care of it.
+		// Similarly, image urls don't have a meaningful <title> attribute so downloading it
+		// to fetch the title is a waste of bandwidth.
+		if (!CheckIf.isUrl(clipboardText) || CheckIf.isImage(clipboardText)) {
+			editor.replaceSelection(clipboardText);
+			return;
+		}
 
-    var clipboardText = await navigator.clipboard.readText();
-    if (clipboardText == null || clipboardText == "") return;
+		let selectedText = (
+			EditorExtensions.getSelectedText(editor) || ""
+		).trim();
+		if (selectedText && !this.settings.shouldReplaceSelection) {
+			// If there is selected text and shouldReplaceSelection is false, do not fetch title
+			editor.replaceSelection(clipboardText);
+			return;
+		}
 
-    // If its not a URL, we return false to allow the default paste handler to take care of it.
-    // Similarly, image urls don't have a meaningful <title> attribute so downloading it
-    // to fetch the title is a waste of bandwidth.
-    if (!CheckIf.isUrl(clipboardText) || CheckIf.isImage(clipboardText)) {
-      editor.replaceSelection(clipboardText);
-      return;
-    }
+		// If it looks like we're pasting the url into a markdown link already, don't fetch title
+		// as the user has already probably put a meaningful title, also it would lead to the title
+		// being inside the link.
+		if (
+			CheckIf.isMarkdownLinkAlready(editor) ||
+			CheckIf.isAfterQuote(editor)
+		) {
+			editor.replaceSelection(clipboardText);
+			return;
+		}
 
-    let selectedText = (EditorExtensions.getSelectedText(editor) || "").trim();
-    if (selectedText && !this.settings.shouldReplaceSelection) {
-      // If there is selected text and shouldReplaceSelection is false, do not fetch title
-      editor.replaceSelection(clipboardText);
-      return;
-    }
+		// At this point we're just pasting a link in a normal fashion, fetch its title.
+		this.convertUrlToTitledLink(editor, clipboardText);
+		return;
+	}
 
-    // If it looks like we're pasting the url into a markdown link already, don't fetch title
-    // as the user has already probably put a meaningful title, also it would lead to the title
-    // being inside the link.
-    if (CheckIf.isMarkdownLinkAlready(editor) || CheckIf.isAfterQuote(editor)) {
-      editor.replaceSelection(clipboardText);
-      return;
-    }
+	async pasteUrlWithTitle(
+		clipboard: ClipboardEvent,
+		editor: Editor
+	): Promise<void> {
+		if (!this.settings.enhanceDefaultPaste) {
+			return;
+		}
 
-    // At this point we're just pasting a link in a normal fashion, fetch its title.
-    this.convertUrlToTitledLink(editor, clipboardText);
-    return;
-  }
+		// Only attempt fetch if online
+		if (!navigator.onLine) return;
 
-  async pasteUrlWithTitle(clipboard: ClipboardEvent, editor: Editor): Promise<void> {
-    if (!this.settings.enhanceDefaultPaste) {
-      return;
-    }
+		let clipboardText = clipboard.clipboardData.getData("text/plain");
+		if (clipboardText === null || clipboardText === "") return;
 
-    // Only attempt fetch if online
-    if (!navigator.onLine) return;
+		// If its not a URL, we return false to allow the default paste handler to take care of it.
+		// Similarly, image urls don't have a meaningful <title> attribute so downloading it
+		// to fetch the title is a waste of bandwidth.
+		if (!CheckIf.isUrl(clipboardText) || CheckIf.isImage(clipboardText)) {
+			return;
+		}
 
-    let clipboardText = clipboard.clipboardData.getData("text/plain");
-    if (clipboardText === null || clipboardText === "") return;
+		let selectedText = (
+			EditorExtensions.getSelectedText(editor) || ""
+		).trim();
+		if (selectedText && !this.settings.shouldReplaceSelection) {
+			// If there is selected text and shouldReplaceSelection is false, do not fetch title
+			return;
+		}
 
-    // If its not a URL, we return false to allow the default paste handler to take care of it.
-    // Similarly, image urls don't have a meaningful <title> attribute so downloading it
-    // to fetch the title is a waste of bandwidth.
-    if (!CheckIf.isUrl(clipboardText) || CheckIf.isImage(clipboardText)) {
-      return;
-    }
+		// We've decided to handle the paste, stop propagation to the default handler.
+		clipboard.stopPropagation();
+		clipboard.preventDefault();
 
-    let selectedText = (EditorExtensions.getSelectedText(editor) || "").trim();
-    if (selectedText && !this.settings.shouldReplaceSelection) {
-      // If there is selected text and shouldReplaceSelection is false, do not fetch title
-      return;
-    }
+		// If it looks like we're pasting the url into a markdown link already, don't fetch title
+		// as the user has already probably put a meaningful title, also it would lead to the title
+		// being inside the link.
+		if (
+			CheckIf.isMarkdownLinkAlready(editor) ||
+			CheckIf.isAfterQuote(editor)
+		) {
+			editor.replaceSelection(clipboardText);
+			return;
+		}
 
-    // We've decided to handle the paste, stop propagation to the default handler.
-    clipboard.stopPropagation();
-    clipboard.preventDefault();
+		// At this point we're just pasting a link in a normal fashion, fetch its title.
+		this.convertUrlToTitledLink(editor, clipboardText);
+		return;
+	}
 
-    // If it looks like we're pasting the url into a markdown link already, don't fetch title
-    // as the user has already probably put a meaningful title, also it would lead to the title
-    // being inside the link.
-    if (CheckIf.isMarkdownLinkAlready(editor) || CheckIf.isAfterQuote(editor)) {
-      editor.replaceSelection(clipboardText);
-      return;
-    }
+	async isBlacklisted(url: string): Promise<boolean> {
+		await this.loadSettings();
+		this.blacklist = this.settings.websiteBlacklist
+			.split(/,|\n/)
+			.map(s => s.trim())
+			.filter(s => s.length > 0);
+		return this.blacklist.some(site => url.contains(site));
+	}
 
-    // At this point we're just pasting a link in a normal fashion, fetch its title.
-    this.convertUrlToTitledLink(editor, clipboardText);
-    return;
-  }
+	async convertUrlToTitledLink(editor: Editor, url: string): Promise<void> {
+		if (await this.isBlacklisted(url)) {
+			let domain = new URL(url).hostname;
+			editor.replaceSelection(`[${domain}](${url})`);
+			return;
+		}
 
-  async isBlacklisted(url: string): Promise<boolean> {
-    await this.loadSettings();
-    this.blacklist = this.settings.websiteBlacklist.split(/,|\n/).map(s => s.trim()).filter(s => s.length > 0)
-    return this.blacklist.some(site => url.contains(site))
-  }
+		// Generate a unique id for find/replace operations for the title.
+		const pasteId = `Fetching Title#${this.createBlockHash()}`;
 
-  async convertUrlToTitledLink(editor: Editor, url: string): Promise<void> {
-    if (await this.isBlacklisted(url)) {
-      let domain = new URL(url).hostname;
-      editor.replaceSelection(`[${domain}](${url})`);
-      return;
-    }
+		// Instantly paste so you don't wonder if paste is broken
+		editor.replaceSelection(`[${pasteId}](${url})`);
 
-    // Generate a unique id for find/replace operations for the title.
-    const pasteId = `Fetching Title#${this.createBlockHash()}`;
+		// Fetch title from site, replace Fetching Title with actual title
+		const title = await this.fetchUrlTitle(url);
+		const escapedTitle = this.escapeMarkdown(title);
 
-    // Instantly paste so you don't wonder if paste is broken
-    editor.replaceSelection(`[${pasteId}](${url})`);
+		const text = editor.getValue();
 
-    // Fetch title from site, replace Fetching Title with actual title
-    const title = await this.fetchUrlTitle(url);
-    const escapedTitle = this.escapeMarkdown(title);
+		const start = text.indexOf(pasteId);
+		if (start < 0) {
+			console.log(
+				`Unable to find text "${pasteId}" in current editor, bailing out; link ${url}`
+			);
+		} else {
+			const end = start + pasteId.length;
+			const startPos = EditorExtensions.getEditorPositionFromIndex(
+				text,
+				start
+			);
+			const endPos = EditorExtensions.getEditorPositionFromIndex(
+				text,
+				end
+			);
 
-    const text = editor.getValue();
+			editor.replaceRange(escapedTitle, startPos, endPos);
+		}
+	}
 
-    const start = text.indexOf(pasteId);
-    if (start < 0) {
-      console.log(
-        `Unable to find text "${pasteId}" in current editor, bailing out; link ${url}`
-      );
-    } else {
-      const end = start + pasteId.length;
-      const startPos = EditorExtensions.getEditorPositionFromIndex(text, start);
-      const endPos = EditorExtensions.getEditorPositionFromIndex(text, end);
+	escapeMarkdown(text: string): string {
+		var unescaped = text.replace(/\\(\*|_|`|~|\\|\[|\])/g, "$1"); // unescape any "backslashed" character
+		var escaped = unescaped.replace(/(\*|_|`|~|\\|\[|\])/g, "\\$1"); // escape *, _, `, ~, \, [, ]
+		return escaped;
+	}
 
-      editor.replaceRange(escapedTitle, startPos, endPos);
-    }
-  }
+	async fetchUrlTitle(url: string): Promise<string> {
+		try {
+			const title = await getPageTitle(url);
+			return title.replace(/(\r\n|\n|\r)/gm, "").trim();
+		} catch (error) {
+			// console.error(error)
+			return "Site Unreachable";
+		}
+	}
 
-  escapeMarkdown(text: string): string {
-    var unescaped = text.replace(/\\(\*|_|`|~|\\|\[|\])/g, '$1'); // unescape any "backslashed" character
-    var escaped = unescaped.replace(/(\*|_|`|~|\\|\[|\])/g, '\\$1'); // escape *, _, `, ~, \, [, ]
-    return escaped;
-  }
+	public getUrlFromLink(link: string): string {
+		let urlRegex = new RegExp(DEFAULT_SETTINGS.linkRegex);
+		return urlRegex.exec(link)[2];
+	}
 
-  async fetchUrlTitle(url: string): Promise<string> {
-    try {
-      const title = await getPageTitle(url);
-      return title.replace(/(\r\n|\n|\r)/gm, "").trim();
-    } catch (error) {
-      // console.error(error)
-      return "Site Unreachable";
-    }
-  }
+	// Custom hashid by @shabegom
+	private createBlockHash(): string {
+		let result = "";
+		var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+		var charactersLength = characters.length;
+		for (var i = 0; i < 4; i++) {
+			result += characters.charAt(
+				Math.floor(Math.random() * charactersLength)
+			);
+		}
+		return result;
+	}
 
-  public getUrlFromLink(link: string): string {
-    let urlRegex = new RegExp(DEFAULT_SETTINGS.linkRegex);
-    return urlRegex.exec(link)[2];
-  }
+	onunload() {
+		console.log("unloading obsidian-auto-link-title");
+	}
 
-  // Custom hashid by @shabegom
-  private createBlockHash(): string {
-    let result = "";
-    var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < 4; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
 
-  onunload() {
-    console.log("unloading obsidian-auto-link-title");
-  }
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
--- settings.ts
+++ settings.ts
@@ -2,87 +2,83 @@ import AutoLinkTitle from "main";
 import { App, PluginSettingTab, Setting } from "obsidian";
 
 export interface AutoLinkTitleSettings {
-  regex: RegExp;
-  lineRegex: RegExp;
-  linkRegex: RegExp;
-  linkLineRegex: RegExp;
-  imageRegex: RegExp;
-  shouldReplaceSelection: boolean;
-  enhanceDefaultPaste: boolean;
-  websiteBlacklist: string;
+	regex: RegExp;
+	lineRegex: RegExp;
+	linkRegex: RegExp;
+	linkLineRegex: RegExp;
+	imageRegex: RegExp;
+	shouldReplaceSelection: boolean;
+	enhanceDefaultPaste: boolean;
+	websiteBlacklist: string;
 }
 
 export const DEFAULT_SETTINGS: AutoLinkTitleSettings = {
-  regex:
-    /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/i,
-  lineRegex:
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
-  linkRegex:
-    /^\[([^\[\]]*)\]\((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\)$/i,
-  linkLineRegex:
-    /\[([^\[\]]*)\]\((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\)/gi,
-  imageRegex: /\.(gif|jpe?g|tiff?|png|webp|bmp|tga|psd|ai)$/i,
-  shouldReplaceSelection: true,
-  enhanceDefaultPaste: true,
-  websiteBlacklist: "",
+	regex: /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/i,
+	lineRegex: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi,
+	linkRegex: /^\[([^\[\]]*)\]\((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\)$/i,
+	linkLineRegex: /\[([^\[\]]*)\]\((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\)/gi,
+	imageRegex: /\.(gif|jpe?g|tiff?|png|webp|bmp|tga|psd|ai)$/i,
+	shouldReplaceSelection: true,
+	enhanceDefaultPaste: true,
+	websiteBlacklist: ""
 };
 
 export class AutoLinkTitleSettingTab extends PluginSettingTab {
-  plugin: AutoLinkTitle;
+	plugin: AutoLinkTitle;
 
-  constructor(app: App, plugin: AutoLinkTitle) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
+	constructor(app: App, plugin: AutoLinkTitle) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
 
-  display(): void {
-    let { containerEl } = this;
+	display(): void {
+		let { containerEl } = this;
 
-    containerEl.empty();
+		containerEl.empty();
 
-    new Setting(containerEl)
-      .setName("Enhance Default Paste")
-      .setDesc(
-        "Fetch the link title when pasting a link in the editor with the default paste command"
-      )
-      .addToggle((val) =>
-        val
-          .setValue(this.plugin.settings.enhanceDefaultPaste)
-          .onChange(async (value) => {
-            console.log(value);
-            this.plugin.settings.enhanceDefaultPaste = value;
-            await this.plugin.saveSettings();
-          })
-      );
+		new Setting(containerEl)
+			.setName("Enhance Default Paste")
+			.setDesc(
+				"Fetch the link title when pasting a link in the editor with the default paste command"
+			)
+			.addToggle(val =>
+				val
+					.setValue(this.plugin.settings.enhanceDefaultPaste)
+					.onChange(async value => {
+						console.log(value);
+						this.plugin.settings.enhanceDefaultPaste = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
-    new Setting(containerEl)
-      .setName("Replace Selection")
-      .setDesc(
-        "Whether to replace a text selection with link and fetched title"
-      )
-      .addToggle((val) =>
-        val
-          .setValue(this.plugin.settings.shouldReplaceSelection)
-          .onChange(async (value) => {
-            console.log(value);
-            this.plugin.settings.shouldReplaceSelection = value;
-            await this.plugin.saveSettings();
-          })
-      );
+		new Setting(containerEl)
+			.setName("Replace Selection")
+			.setDesc(
+				"Whether to replace a text selection with link and fetched title"
+			)
+			.addToggle(val =>
+				val
+					.setValue(this.plugin.settings.shouldReplaceSelection)
+					.onChange(async value => {
+						console.log(value);
+						this.plugin.settings.shouldReplaceSelection = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
-    new Setting(containerEl)
-      .setName("Website Blacklist")
-      .setDesc(
-        "List of strings (comma separated) that disable autocompleting website titles. Can be URLs or arbitrary text."
-      )
-      .addTextArea((val) =>
-        val
-          .setValue(this.plugin.settings.websiteBlacklist)
-          .setPlaceholder("localhost, tiktok.com")
-          .onChange(async (value) => {
-            this.plugin.settings.websiteBlacklist = value;
-            await this.plugin.saveSettings();
-          })
-      );
-  }
+		new Setting(containerEl)
+			.setName("Website Blacklist")
+			.setDesc(
+				"List of strings (comma separated) that disable autocompleting website titles. Can be URLs or arbitrary text."
+			)
+			.addTextArea(val =>
+				val
+					.setValue(this.plugin.settings.websiteBlacklist)
+					.setPlaceholder("localhost, tiktok.com")
+					.onChange(async value => {
+						this.plugin.settings.websiteBlacklist = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
