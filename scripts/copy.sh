a5682634348058a43fbb27dd0a71380c8bb0e88a
Daniel Yim <danielyim@gmail.com>
2023-12-24 18:57:34 -0800

    Add local dev script

---
+++ scripts/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copy to local Obsidian vault
+OBSIDIAN_PLUGIN_PATH=~/Obsidian/Main/Main/.obsidian/plugins
+cp main.js $OBSIDIAN_PLUGIN_PATH/obsidian-named-links/
+cp styles.css $OBSIDIAN_PLUGIN_PATH/obsidian-named-links/
+cp manifest.json $OBSIDIAN_PLUGIN_PATH/obsidian-named-links/
